99d5cb7038f37693395cbbe70325462c3c81ee31
Ines Rojas <ia.rojas2@uniandes.edu.co>
2023-04-28 21:08:09 -0500

    contar plantas

--- src/app/planta/lista-planta/lista-planta.component.ts
+++ src/app/planta/lista-planta/lista-planta.component.ts
@@ -10,13 +10,31 @@ import { ListaPlantaService } from './lista-planta.service';
 export class ListaPlantaComponent {
 
   plantas: Array<Planta> = [];
+  interior : number = 0
+  exterior : Number = 0
 
   constructor(private listaPlantaService: ListaPlantaService) { }
 
   getPlantas() {
     this.listaPlantaService.getPlantas().subscribe(plantas => {
       this.plantas = plantas;
+      this.contarPlantas(plantas)
     });
+  }
+
+  contarPlantas(plantas: Array<Planta>) {
+    var interior = 0
+    var exterior = 0
+    plantas.forEach(function(p){
+      if(p.tipo == "Interior"){
+        interior++;
+      }
+      if(p.tipo == "Exterior"){
+        exterior++;
+      }
+    })
+    this.interior = interior;
+    this.exterior = exterior;
   }
 
   ngOnInit(): void {
